7430b282bf4a651e2ec8a20fd5151cb8b83dd116
Xiang <xiangqingkou@163.com>
2023-04-26 22:20:45 +0800

    4.26 晚

--- src/com/carl/qqcommon/User.java
+++ src/com/carl/qqcommon/User.java
@@ -5,11 +5,11 @@ import java.io.Serializable;
 /**
  * @author Carl
  * @version 1.0
- * @date 2023/4/25 22:45
  * // 表示用户信息
  */
 public class User implements Serializable{
 
+    public User(){}
     private static final long serialVersionUID = 1L;
     private String userId;//用户Id
     private String password;//用户密码
